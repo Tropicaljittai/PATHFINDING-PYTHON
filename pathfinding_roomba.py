6ab3e64a9f077595074459d6598c6349df414519
Joseph Ruys <93302274+Tropicaljittai@users.noreply.github.com>
2023-12-16 17:26:58 +0700

    -

--- pathfinding_roomba.py
+++ pathfinding_roomba.py
@@ -1,6 +1,7 @@
 import pygame, sys 
 from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
+from pathfinding.finder.dijkstra import DijkstraFinder
 from pathfinding.core.diagonal_movement import DiagonalMovement
 
 class Pathfinder:
@@ -41,7 +42,7 @@ class Pathfinder:
 		end = self.grid.node(end_x,end_y) 
 
 		# path
-		finder = AStarFinder(diagonal_movement = DiagonalMovement.always)
+		finder = DijkstraFinder(diagonal_movement = DiagonalMovement.always)
 		self.path,_ = finder.find_path(start,end,self.grid)
 		self.grid.cleanup()
 		self.roomba.sprite.set_path(self.path)
@@ -66,11 +67,23 @@ class Pathfinder:
 
 class Roomba(pygame.sprite.Sprite):
 	def __init__(self,empty_path):
-
+		self.spriteCounter = 0
+		self.spriteNum = 1
 		# basic
 		super().__init__()
-		self.image = pygame.image.load('roomba.png').convert_alpha()
-		self.rect = self.image.get_rect(center = (60,60))
+		self.face = ""
+		self.sprites = []
+		self.sprites.append(pygame.image.load('up1.png'))
+		self.sprites.append(pygame.image.load('up2.png'))
+		self.sprites.append(pygame.image.load('down1.png'))
+		self.sprites.append(pygame.image.load('down2.png'))
+		self.sprites.append(pygame.image.load('left1.png'))
+		self.sprites.append(pygame.image.load('left2.png'))
+		self.sprites.append(pygame.image.load('right1.png'))
+		self.sprites.append(pygame.image.load('right2.png'))
+		self.current_sprite = 0
+		self.image = self.sprites[self.current_sprite]
+		self.rect = self.image.get_rect(center = (320, 256))
 
 		# movement 
 		self.pos = self.rect.center
@@ -81,6 +94,8 @@ class Roomba(pygame.sprite.Sprite):
 		self.path = []
 		self.collision_rects = []
 		self.empty_path = empty_path
+
+	
 
 	def get_coord(self):
 		col = self.rect.centerx // 32
@@ -102,6 +117,7 @@ class Roomba(pygame.sprite.Sprite):
 				self.collision_rects.append(rect)
 
 	def get_direction(self):
+		self.get_current_direction()
 		if self.collision_rects:
 			start = pygame.math.Vector2(self.pos)
 			end = pygame.math.Vector2(self.collision_rects[0].center)
@@ -119,7 +135,53 @@ class Roomba(pygame.sprite.Sprite):
 		else:
 			self.empty_path()
 
+	def get_current_direction(self):
+		
+		angle = pygame.math.Vector2(0, -1).angle_to(self.direction)
+		if angle >= 355 or angle <= 10 :
+			self.face = "up"
+		if (angle >= 15 and angle <= 180) :
+			self.face = "right"
+		if angle >= 190 and angle <= 345 or angle <= -30:
+			self.face = "left"
+		if angle >= 175 and angle <= 185 :
+			self.face = "down"
+		
+
+
 	def update(self):
+		self.spriteCounter+=1
+		if self.spriteCounter > 17:
+			if self.spriteNum == 1:
+				self.spriteNum = 2	
+			elif self.spriteNum == 2:
+				self.spriteNum = 1
+			self.spriteCounter = 0
+
+		if self.face == "up":
+			if self.spriteNum == 1:
+				self.current_sprite = 0
+			elif self.spriteNum == 2:
+				self.current_sprite = 1
+		if self.face == "down":
+			if self.spriteNum == 1:
+				self.current_sprite = 2
+			elif self.spriteNum == 2:
+				self.current_sprite = 3
+		if self.face == "right":
+			if self.spriteNum == 1:
+				self.current_sprite = 6
+			elif self.spriteNum == 2:
+				self.current_sprite = 7
+		if self.face == "left":
+			if self.spriteNum == 1:
+				self.current_sprite = 4
+			elif self.spriteNum == 2:
+				self.current_sprite = 5
+
+		print(self.spriteNum)
+
+		self.image = self.sprites[self.current_sprite]
 		self.pos += self.direction * self.speed
 		self.check_collisions()
 		self.rect.center = self.pos
@@ -133,15 +195,15 @@ clock = pygame.time.Clock()
 bg_surf = pygame.image.load('map_2.png').convert()
 matrix = [
 	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1],
-	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1],
-	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,0,0,1,1,1,1],
-	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,0,0,1,1,1,1],
-	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,1,1,1,0,0,1,1,1,1],
-	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,1,1,1,0,0,1,1,1,1],
-	[1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1],
-	[1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,1,1,0,0,1,0,0,1,1,1,1],
-	[1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,1,1,0,0,1,0,0,1,1,1,1],
+    [0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1,1,1,0],
+    [0,1,0,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,0,0,1],
+    [0,0,0,0,0,0,1,0,1,0,0,1,1,0,0,0,1,1,1,0,0,0,0,0,1,0,1,0,0,1],
+    [0,0,0,0,0,0,1,0,1,0,0,1,1,0,0,0,0,1,1,1,1,1,1,0,1,1,1,1,1,1],
+    [0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,0,1,0,0],
+    [0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,0,0,0,0,0,1,1,1,0,1,1,1,1,0,0],
+    [0,1,1,1,1,1,1,1,1,1,1,0,1,1,1,0,0,0,0,0,1,1,1,0,1,1,1,1,0,1],
+    [0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,0,0,1,0,0,1,1],
+    [0,1,1,1,0,0,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1],
 
 	[0,1,0,1,0,0,1,1,0,1,1,1,1,1,1,1,1,0,0,0,0,1,0,0,0,1,1,1,1,0],
 	[1,0,1,1,1,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -163,9 +225,7 @@ while True:
 			sys.exit()
 		if event.type == pygame.MOUSEBUTTONDOWN:
 			pathfinder.create_path()
-
 	screen.blit(bg_surf,(0,0))
 	pathfinder.update()
-
 	pygame.display.update()
 	clock.tick(60)
